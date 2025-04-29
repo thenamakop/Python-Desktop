1a982b246a3030843471a84a4705d7a30b587088
Maulik Gupta <maulikgupta21@gmail.com>
2025-04-29 19:55:42 +0530

    Moved Assigment PDF into Assignment Folder

---
+++ class/lab/FH/assignment/geometry.py
@@ -0,0 +1,9 @@
+import math
+pi = math.pi
+print(pi)
+
+def calculate_area_circle(radius):
+    return pi*radius*radius
+
+def calculate_area_rectangle(length,width):
+    return length*width
---
+++ class/lab/FH/assignment/main.py
@@ -0,0 +1,8 @@
+import geometry
+
+radius = float(input("Enter Radius: "))
+print(f"The Aread OF the circle is {geometry.calculate_area_circle(radius)}")
+
+length = float(input("Enter Length: "))
+width = float(input("Enter Width: "))
+print(f"The Area of the Rectangle is {geometry.calculate_area_rectangle(length,width)}")
